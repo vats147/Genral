5755fc2f02d69d6299265edda0b9de0ec8f190e7
vats147 <vatsalmangukiya9003@gmail.com>
2023-01-16 09:15:37 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head></head>
+
+<body>
+
+<div class="testbackground">
+    <a href="/another-page">Another Page</a>
+
+    <h1>Hello there!</h1>
+
+    <?php include __DIR__ . '/another-page/index.php'; ?>
+</div>
+</body>
+</html>
